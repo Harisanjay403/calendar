de6a74246dffced2e0a329068873380457c1984b
Harisanjay403 <168261655+Harisanjay403@users.noreply.github.com>
2024-07-11 16:34:00 +0530

    Update calendar.jsx

--- calendar.jsx
+++ calendar.jsx
@@ -1,4 +1,4 @@
-
+ 
 import {useState} from "react";
 
 const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat",]    
@@ -7,9 +7,15 @@ export default function App() {
     const [selectedDate, setSelectedDate]=useState(new Date())
     const daysInMonth = ()=>{
         const daysArray=[];
-        const firstDay = new Date(selectedDate.getFullYear(),selectedDate.getFullMonth(),1)
-        const lastDay = new Date(selectedDate.getFullYear(),selectedDate.getFullMonth() +1, 0)
+        const firstDay = new Date(selectedDate.getFullYear(),selectedDate.getMonth(),1)
+        const lastDay = new Date(selectedDate.getFullYear(),selectedDate.getMonth() +1, 0)
+        for (i=0; i<firstDay.getDay();i++){
+            daysArray.push(null);
+        }
 
+        for(i=1; i<=lastDay.getDate();i++){
+            daysArray.push(new Date(selectedDate.getFullYear(),selectedDate.getMonth(),i))
+        }
         return daysArray
     }
   
@@ -18,14 +24,27 @@ export default function App() {
           <div className="calendar">
               <div className="header">
                   <button>&#60;</button>
-                  <select></select>
-                  <select></select>
+                  <select value={selectedDate.getMonth()}>
+                      {months.map((month,index)=>(
+                              <option key={index} value={index}>{month}</option>
+                          ))}
+                  </select>
+                  <select value={selectedDate.getFullYear()}>
+                      {Array.from({length: 10}, (_, i) =>selectedDate.getFullYear() - 5 + i ).map((year)=>(
+                      <option key={year} value={year}>{year}</option>
+                      ))}
+                  </select>
                   <button>&gt;</button>
               </div>
 
               <div className="daysOfWeek">
                   {daysOfWeek.map((day)=>(
                       <div>{day}</div>
+                  ))}
+              </div>
+              <div className="days">
+                  {daysInMonth().map((day, index)=>(
+                      <div key={index} className={day?"day":"empty"}>{day ?day.getDate() : ""}</div>
                   ))}
               </div>
           </div>
